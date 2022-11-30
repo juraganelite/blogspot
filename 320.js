29aa9531c8c0c50fd087a02e99edd320404b8dab
juraganelite <101795987+juraganelite@users.noreply.github.com>
2022-11-30 07:48:27 +0700

    Create 320.js

---
+++ 320.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '95579c685120ba7de415d150af70ca76',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://bringsconserve.com/95579c685120ba7de415d150af70ca76/invoke.js"></scr' + 'ipt>');
